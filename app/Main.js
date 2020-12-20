77a9f425d81bbd0f05483e83388d73fd11e7dd54
KHwong12 <khwongk12@gmail.com>
2020-12-20 18:18:43 +0800

    Use Online WebMap instead of Map

--- app/Main.js
+++ app/Main.js
@@ -1,36 +1,27 @@
 require([
-    "esri/Map",
+    "esri/WebMap",
     "esri/views/MapView",
     "esri/layers/FeatureLayer",
     "esri/geometry/Extent"
 ], function (
-    Map,
+    WebMap,
     MapView,
     FeatureLayer,
     Extent
 ) {
     // Load webscene and display it in a SceneView
-    const map = new Map({
-        basemap: "dark-gray-vector"
-    });
-    // create the SceneView
-    const view = new MapView({
-        container: "viewDiv",
-        map: map,
-        zoom: 10,
-        center: [114.145, 22.360] // lon, lat
-    });
-
-
-    const layer = new FeatureLayer({
-        portalItem: {   // autocasts as new PortalItem
-            id: "d32594d7352e40a291603bfd6a57f0fa"
+    const webmap = new WebMap({
+        portalItem: { // autocasts as new PortalItem()
+            id: "01807d9d7e954671bcfbcbe64290ac92"
         }
     });
 
-    map.add(layer);  // adds the layer to the map
-
-
-
+    // create the SceneView
+    const view = new MapView({
+        container: "viewDiv",
+        map: webmap,
+        zoom: 10,
+        center: [114.145, 22.360] // lon, lat
+    });
 
 });
