0473bc941b37a04f5cdbfbf6b4752fc0c2aeb2f1
KHwong12 <khwongk12@gmail.com>
2020-12-20 18:40:48 +0800

    Complete basic setup wih WebMap

--- app/Main.js
+++ app/Main.js
@@ -2,14 +2,24 @@ require([
     "esri/WebMap",
     "esri/views/MapView",
     "esri/layers/FeatureLayer",
-    "esri/geometry/Extent"
+    "esri/layers/GraphicsLayer",
+    "esri/widgets/Sketch/SketchViewModel",
+    "esri/widgets/Slider",
+    "esri/geometry/geometryEngine",
+    "esri/Graphic",
+    "esri/core/promiseUtils"
 ], function (
     WebMap,
     MapView,
     FeatureLayer,
-    Extent
+    GraphicsLayer,
+    SketchViewModel,
+    Slider,
+    geometryEngine,
+    Graphic,
+    promiseUtils
 ) {
-    // Load webscene and display it in a SceneView
+    // Load webmap and display it in a SceneView
     const webmap = new WebMap({
         portalItem: { // autocasts as new PortalItem()
             id: "01807d9d7e954671bcfbcbe64290ac92"
@@ -24,4 +34,32 @@ require([
         center: [114.145, 22.360] // lon, lat
     });
 
+    window.view = view;
+
+    // add a GraphicsLayer for the sketches and the buffer
+    const sketchLayer = new GraphicsLayer();
+    const bufferLayer = new GraphicsLayer();
+    view.map.addMany([bufferLayer, sketchLayer]);
+
+    let webLayer = null;
+    let webLayerView = null;
+    let bufferSize = 0;
+
+    // Assign scene layer once webmap is loaded and initialize UI
+    webmap.load().then(function () {
+        webLayer = webmap.layers.find(function (layer) {
+            return layer.title === "OZP_Nov2019_Sim";
+        });
+        // Fetch all fields
+        // https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-FeatureLayer.html#outFields
+        webLayer.outFields = ["*"];
+
+        view.whenLayerView(webLayer).then(function (layerView) {
+            webLayerView = layerView;
+            queryDiv.style.display = "block";
+        });
+    });
+
+    view.ui.add([queryDiv], "bottom-left");
+    view.ui.add([resultDiv], "top-right");
 });
