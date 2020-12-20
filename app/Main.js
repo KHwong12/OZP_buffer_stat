044f42a661c99cb035b18780632a405b5509fcaf
KHwong12 <khwongk12@gmail.com>
2020-12-21 00:34:12 +0800

    Add chart visualising number of zoning pieces in buffer

--- app/Main.js
+++ app/Main.js
@@ -178,7 +178,7 @@ require([
         sketchLayer.removeAll();
         bufferLayer.removeAll();
         clearHighlighting();
-/*        clearCharts();*/
+        clearCharts();
         resultDiv.style.display = "none";
     }
 
@@ -272,12 +272,129 @@ require([
 
     function queryStatistics() {
         // TODO
+        const statDefinitions = [
+            {
+                onStatisticField:
+                    "CASE WHEN ZONE_MAS = 'R(A)' THEN 1 ELSE 0 END",
+                outStatisticFieldName: "zone_RA",
+                statisticType: "sum"
+            },
+            {
+                onStatisticField:
+                    "CASE WHEN ZONE_MAS = 'R(B)' THEN 1 ELSE 0 END",
+                outStatisticFieldName: "zone_RB",
+                statisticType: "sum"
+            },
+            {
+                onStatisticField:
+                    "CASE WHEN ZONE_MAS = 'R(C)' THEN 1 ELSE 0 END",
+                outStatisticFieldName: "zone_RC",
+                statisticType: "sum"
+            },
+            {
+                onStatisticField:
+                    "CASE WHEN ZONE_MAS = 'G/IC' THEN 1 ELSE 0 END",
+                outStatisticFieldName: "zone_GIC",
+                statisticType: "sum"
+            },
+            {
+                onStatisticField:
+                    "CASE WHEN ZONE_MAS = 'O' THEN 1 ELSE 0 END",
+                outStatisticFieldName: "zone_O",
+                statisticType: "sum"
+            },
+            {
+                onStatisticField:
+                    "CASE WHEN ZONE_MAS = 'C' THEN 1 ELSE 0 END",
+                outStatisticFieldName: "zone_C",
+                statisticType: "sum"
+            }
+        ];
+
+        const query = webLayerView.createQuery();
+        query.geometry = sketchGeometry;
+        query.distance = bufferSize;
+        query.outStatistics = statDefinitions;
+
+        return webLayerView.queryFeatures(query).then(function (result) {
+            const allStats = result.features[0].attributes;
+
+            console.log(result);
+
+            updateChart(zoningNumberChart, [
+                allStats.zone_RA,
+                allStats.zone_RB,
+                allStats.zone_RC,
+                allStats.zone_GIC,
+                allStats.zone_O,
+                allStats.zone_C,
+            ]);
+        }, console.error);
     }
 
-    function createZoningAreaChart() {
+    function createzoningNumberChart() {
 
-        const zoningAreaCanvas = document.getElementById("zoning-area-chart");
+        const zoningNumberCanvas = document.getElementById("zoning-number-chart");
 
-
+        zoningNumberChart = new Chart(zoningNumberCanvas.getContext("2d"), {
+            type: "horizontalBar",
+            data: {
+                labels: [
+                    "R(A)",
+                    "R(B)",
+                    "R(C)",
+                    "G/IC",
+                    "O",
+                    "C"
+                ],
+                datasets: [
+                    {
+                        label: "Zoning",
+                        backgroundColor: "#149dcf",
+                        stack: "Stack 0",
+                        data: [0, 0, 0, 0, 0, 0]
+                    }
+                ]
+            },
+            options: {
+                responsive: false,
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: "Zoning"
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            stacked: true,
+                            ticks: {
+                                beginAtZero: true,
+                                precision: 0
+                            }
+                        }
+                    ],
+                    yAxes: [
+                        {
+                            stacked: true
+                        }
+                    ]
+                }
+            }
+        });
     }
+
+    // Updates the given chart with new data
+    function updateChart(chart, dataValues) {
+        chart.data.datasets[0].data = dataValues;
+        chart.update();
+    }
+
+    function clearCharts() {
+        updateChart(zoningNumberChart, [0, 0, 0, 0, 0, 0]);
+        document.getElementById("count").innerHTML = 0;
+    }
+
+    createzoningNumberChart();
 });
