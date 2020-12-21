eae21406ae18ecf60b7b4abdc2bc254552c6431c
Kenneth Wong <khwongk12@gmail.com>
2020-12-21 20:09:21 +0800

    Move chart creating function to separate file

--- app/Main.js
+++ app/Main.js
@@ -24,7 +24,7 @@ require([
         portalItem: { // autocasts as new PortalItem()
             id: "01807d9d7e954671bcfbcbe64290ac92"
         },
-        basemap: "dark-gray-vector"	
+        basemap: "dark-gray-vector"
     });
 
     // create the MapView
@@ -69,8 +69,9 @@ require([
     view.ui.add([queryDiv], "bottom-left");
     view.ui.add([resultDiv], "top-right");
 
-    // use SketchViewModel to draw polygons that are used as a query
+    // use SketchViewModel to draw polygons for spatial query
     let sketchGeometry = null;
+
     const sketchViewModel = new SketchViewModel({
         layer: sketchLayer,
         defaultUpdateOptions: {
@@ -164,7 +165,7 @@ require([
         bufferSize = event.value;
         console.log("Querying the geometry with buffer size of ", bufferSize);
         runQuery();
-    }  
+    }
 
     // Listener of "Clear Geometry" Button
     document
@@ -239,7 +240,7 @@ require([
         }
     }
 
-    
+
     var highlightHandle = null;
 
     function clearHighlighting() {
@@ -267,134 +268,6 @@ require([
         return webLayerView.queryObjectIds(query).then(highlightGeometries);
     }
 
-
-    /// *** Create Charts *** ///
-
-    function queryStatistics() {
-        // TODO
-        const statDefinitions = [
-            {
-                onStatisticField:
-                    "CASE WHEN ZONE_MAS = 'R(A)' THEN 1 ELSE 0 END",
-                outStatisticFieldName: "zone_RA",
-                statisticType: "sum"
-            },
-            {
-                onStatisticField:
-                    "CASE WHEN ZONE_MAS = 'R(B)' THEN 1 ELSE 0 END",
-                outStatisticFieldName: "zone_RB",
-                statisticType: "sum"
-            },
-            {
-                onStatisticField:
-                    "CASE WHEN ZONE_MAS = 'R(C)' THEN 1 ELSE 0 END",
-                outStatisticFieldName: "zone_RC",
-                statisticType: "sum"
-            },
-            {
-                onStatisticField:
-                    "CASE WHEN ZONE_MAS = 'G/IC' THEN 1 ELSE 0 END",
-                outStatisticFieldName: "zone_GIC",
-                statisticType: "sum"
-            },
-            {
-                onStatisticField:
-                    "CASE WHEN ZONE_MAS = 'O' THEN 1 ELSE 0 END",
-                outStatisticFieldName: "zone_O",
-                statisticType: "sum"
-            },
-            {
-                onStatisticField:
-                    "CASE WHEN ZONE_MAS = 'C' THEN 1 ELSE 0 END",
-                outStatisticFieldName: "zone_C",
-                statisticType: "sum"
-            }
-        ];
-
-        const query = webLayerView.createQuery();
-        query.geometry = sketchGeometry;
-        query.distance = bufferSize;
-        query.outStatistics = statDefinitions;
-
-        return webLayerView.queryFeatures(query).then(function (result) {
-            const allStats = result.features[0].attributes;
-
-            console.log(result);
-
-            updateChart(zoningNumberChart, [
-                allStats.zone_RA,
-                allStats.zone_RB,
-                allStats.zone_RC,
-                allStats.zone_GIC,
-                allStats.zone_O,
-                allStats.zone_C,
-            ]);
-        }, console.error);
-    }
-
-    function createzoningNumberChart() {
-
-        const zoningNumberCanvas = document.getElementById("zoning-number-chart");
-
-        zoningNumberChart = new Chart(zoningNumberCanvas.getContext("2d"), {
-            type: "horizontalBar",
-            data: {
-                labels: [
-                    "R(A)",
-                    "R(B)",
-                    "R(C)",
-                    "G/IC",
-                    "O",
-                    "C"
-                ],
-                datasets: [
-                    {
-                        label: "Zoning",
-                        backgroundColor: "#149dcf",
-                        stack: "Stack 0",
-                        data: [0, 0, 0, 0, 0, 0]
-                    }
-                ]
-            },
-            options: {
-                responsive: false,
-                legend: {
-                    display: false
-                },
-                title: {
-                    display: true,
-                    text: "Zoning"
-                },
-                scales: {
-                    xAxes: [
-                        {
-                            stacked: true,
-                            ticks: {
-                                beginAtZero: true,
-                                precision: 0
-                            }
-                        }
-                    ],
-                    yAxes: [
-                        {
-                            stacked: true
-                        }
-                    ]
-                }
-            }
-        });
-    }
-
-    // Updates the given chart with new data
-    function updateChart(chart, dataValues) {
-        chart.data.datasets[0].data = dataValues;
-        chart.update();
-    }
-
-    function clearCharts() {
-        updateChart(zoningNumberChart, [0, 0, 0, 0, 0, 0]);
-        document.getElementById("count").innerHTML = 0;
-    }
 
     createzoningNumberChart();
 });
