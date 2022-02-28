5f95a68a88f24767c96355c9c8acf40117f1a728
Kenneth Wong <khwongk12@gmail.com>
2022-03-01 03:15:07 +0800

    chore: Remove obsolete comment

--- app/ui.js
+++ app/ui.js
@@ -1,6 +1,3 @@
-// UI function for fold/unfold side panel
-// https://codepen.io/DeolaJ/pen/xvjbKY
-
 export function changeMenuIcon (sidebar, collapseBtn) {
   if (sidebar.classList.contains("open")) {
     // change the icon from "menu" to "right-padded menu"
@@ -9,6 +6,5 @@ export function changeMenuIcon (sidebar, collapseBtn) {
     collapseBtn.classList.replace("bx-menu-alt-right", "bx-menu");
   }
 }
-
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
