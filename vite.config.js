04be9c6eea32586ed36b4e1ff78f04cbbfa59bde
Kenneth Wong <khwongk12@gmail.com>
2022-09-17 19:10:56 +0800

    dev: Standardise vite config options

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+import { defineConfig } from 'vite'
+
 // vite.config.js
-export default {
+export default defineConfig({
   // config options
 
   // For specifying sub-page when deploying to gh pages
   // https://vitejs.dev/guide/static-deploy.html#github-pages
   base: '/OZP_buffer_stat/'
-}
+})
