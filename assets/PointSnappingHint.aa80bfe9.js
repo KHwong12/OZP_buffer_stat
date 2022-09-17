1c8abc7f91e6b387826c116f290f746382f7d0cd
KHwong12 <khwongk12@gmail.com>
2022-09-17 19:05:34 +0000

    Deploying to gh-pages from @ KHwong12/OZP_buffer_stat@2d8c6b2253d33d97d2d7b33f2f96a5d3b464b186 🚀

---
+++ assets/PointSnappingHint.aa80bfe9.js
@@ -0,0 +1,1 @@
+import{e_ as e,e$ as n}from"./index.c449ba0d.js";class s extends e{constructor(t,o){super(o),this.point=t}equals(t){return t instanceof s&&n(this.point,t.point)}}export{s};
---
+++ assets/jsonContext.216eaf0f.js
@@ -0,0 +1,1 @@
+import{c as r,B as e}from"./index.c449ba0d.js";function l(t){return{origin:"portal-item",url:r(t.itemUrl),portal:t.portal||e.getDefault(),portalItem:t,readResourcePaths:[]}}function a(t){return{origin:"portal-item",messages:[],writtenProperties:[],url:t.itemUrl?r(t.itemUrl):null,portal:t.portal||e.getDefault(),portalItem:t}}export{l as e,a as o};
